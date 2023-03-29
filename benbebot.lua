524810ba73cee607c1fe9678e479a365a279b427
Benbebop <benbebop@hotmail.com>
2023-03-29 16:33:30 -0600

    fix addinvite command

--- benbebot.lua
+++ benbebot.lua
@@ -8,101 +8,18 @@ local enums = discordia.enums
 local benbebot, familyGuy = discordia.Client(), discordia.Client()
 benbebot._logger:setPrefix("BBB") familyGuy._logger:setPrefix("FLG") 
 
+benbebot:defaultCommandCallback(function(interaction)
+	interaction:reply({embed = {
+		description = "couldnt find command, [please report this error](https://github.com/Benbebop/benbebots/issues)"
+	}})
+end)
+
 do -- BENBEBOTS SERVER --
-	
-	local timer, http, xml = require("timer"), require("coro-http"), require("slaxml")
-	
-	local rssFile, fileFormat = appdata.path("rss-timestamps.db"), "zL"
-	local rssToEmbed = {title = "title", description = "description", link = "url", pubDate = "timestamp", enclosure = "enclosure"}
-	local endpoints = {
-		{"Steamworks", "1068657073321169067", "https://store.steampowered.com/feeds/news/group/4145017"}, 
-		{"Garry's Mod", "1068657073321169067", "https://store.steampowered.com/feeds/news/app/4000"},
-		{"Lua", "1068657073321169067", "https://www.lua.org/news.rss"}
-	}
-
-	benbebot:on("ready", function()
-		
-		local latests = {}
-		
-		local data = fs.readFileSync(rssFile)
-		if data then
-			for part in data:gmatch("[^%z]+.....") do
-				local url, timestamp = fileFormat:unpack(part)
-				latests[url] = discordia.Date().fromSeconds(timestamp)
-			end
-		end
-		
-		local function feedFunc(endpoint)
-			latests[endpoint[3]] = latests[endpoint[3]] or discordia.Date(0,0)
-			
-			local success, result, body = pcall(http.request, "GET", endpoint[3])
-			
-			if not success then
-				client:warning("RSS Feed Failed: " .. body)
-				return
-			end
-			
-			local embed, index
-			
-			xml:parser({
-				startElement = function(name)
-					if name == "item" then
-						if embed then benbebot:getChannel(endpoint[2]):send({embed = embed}) end
-						embed = {author = {name = endpoint[1]}}
-					elseif embed then
-						index = rssToEmbed[name]
-					end
-				end,
-				attribute = function(name,value)
-					if index == "enclosure" and name == "url" then
-						embed.image = {url = value}
-					end
-				end,
-				text = function(text)
-					if index then
-						if index == "timestamp" then
-							local date = discordia.Date().fromHeader(text:gsub("[%+%-]%d+", "GMT"))
-							if date:toSeconds() < (latests[endpoint[3]]:toSeconds() + 10) then embed, index = nil, nil return end
-							latests[endpoint[3]] = date
-							embed.timestamp = date:toISO()
-						elseif index == "link" then
-							embed.url = text
-						elseif index == "description" then
-							embed.description = text:gsub("%b<>", ""):gsub("%b[]", ""):sub(1,200) .. "..."
-						elseif index ~= "enclosure" then
-							embed[index] = text
-						end
-					end
-				end
-			}):parse(body,{stripWhitespace=true})
-			
-			local file = fs.openSync(rssFile, "w")
-			local cursor = 0
-			for i,v in pairs(latests) do
-				local data = fileFormat:pack(i, v:toSeconds())
-				fs.writeSync(file, cursor, data)
-				cursor = cursor + #data
-			end
-			fs.closeSync(file)
-
-		end
-		
-		timer.setInterval(1000 * 60 * 60, function()
-			for _,v in ipairs(endpoints) do
-				coroutine.wrap(feedFunc)(v)
-			end
-		end)
-		
-		for _,v in ipairs(endpoints) do
-			coroutine.wrap(feedFunc)(v)
-		end
-		
-	end)
 	
 	-- servers channel
 
-	local inv = benbebot:newSlashCommand("addInvite", "1068640496139915345"):setDescription("add an invite")
-	inv:addOption(enums.applicationCommandOptionType.string, "invite"):setDescription("invite url/code")
+	local inv = benbebot:newSlashCommand("addinvite", "1068640496139915345"):setDescription("add an invite")
+	inv:addOption(enums.applicationCommandOptionType.string, "invite"):setDescription("invite url/code"):setRequired(true)
 	
 	local url = require("url")
 
--- deps/logger-overwrite.lua
+++ deps/logger-overwrite.lua
@@ -1,7 +1,36 @@
 -- sends the bots name to output for multi-bot scripts
 
-local discordia = require("discordia")
+local fs = require('fs')
+
+local date = os.date
+local format = string.format
 local stdout = _G.process.stdout.handle
+local openSync, writeSync = fs.openSync, fs.writeSync
+
+-- local BLACK   = 30
+local RED     = 33
+local GREEN   = 32
+local YELLOW  = 33
+-- local BLUE    = 34
+-- local MAGENTA = 35
+local CYAN    = 36
+-- local WHITE   = 37
+
+local config = {
+        {'[ERR]', RED},
+        {'[WRN]', YELLOW},
+        {'[INF]', GREEN},
+        {'[DBG]', CYAN},
+}
+
+do -- parse config
+        local bold = 1
+        for _, v in ipairs(config) do
+                v[3] = format('\27[%i;%im%s\27[0m', bold, v[2], v[1])
+        end
+end
+
+local discordia = require("discordia")
 
 local Logger = discordia.class.classes.Logger
 
@@ -9,12 +38,21 @@ function Logger:setPrefix(name)
 	self._prefix = string.format("%s | ", name)
 end
 
-local oldLog = Logger.log
+function Logger:log(level, msg, ...)
 
-function Logger:log(...)
-	if self._prefix then
-		stdout:write(self._prefix)
-	end
+        if self._level < level then return end
 
-	return oldLog(self, ...)
+        local tag = config[level]
+        if not tag then return end
+
+        msg = format(msg, ...)
+
+        local d = date(self._dateTime)
+        if self._file then
+                writeSync(self._file, -1, format('%s | %s | %s %s\n', d, tag[1], self._prefix, msg))
+        end
+        stdout:write(format('%s | %s | %s %s\n', d, tag[3], self._prefix, msg))
+
+        return msg
+
 end
