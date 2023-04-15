5316eea678ca8347110924c51aefb4aea9530619
Benbebop <benbebop@hotmail.com>
2023-04-15 12:44:55 -0600

    reaction roles

--- main.lua
+++ main.lua
@@ -50,6 +50,49 @@ do -- BENBEBOTS SERVER --
 
 	end)
 	
+	-- reaction roles
+	
+	benbebot:on("ready", function()
+		benbebot:getChannel("1075203623073632327"):getMessage("1077041796779094096"):setContent(
+[[@everyone You know how this works
+	<@&1075196966654451743> :face_holding_back_tears: - major updates involving the bots
+	<@&1068664164786110554> :video_game: - game server events
+	<@&1075245976543056013> :flag_pl: - polls involving this server
+	<@&1072698350836662392> :sleeping: - get pinged when the bot's pfps are updated
+	<@&1078400699802587136> :skull: - get pinged whenever i feel the urge to kill]]
+		)
+	end)
+
+	local rolesIndex = {
+		["\240\159\165\185"] = "1075196966654451743",
+		["\240\159\142\174"] = "1068664164786110554",
+		["\240\159\135\181\240\159\135\177"] = "1075245976543056013",
+		["\240\159\152\180"] = "1072698350836662392",
+		["\240\159\146\128"] = "1078400699802587136",
+	}
+	
+	local function add(_, messageId, hash, userId)
+		if messageId == "1077041796779094096" then
+			local role = rolesIndex[hash]
+			if not role then return end
+			benbebot:getGuild("1068640496139915345"):getMember(userId):addRole(role)
+		end
+	end
+	
+	local function remove(channel, messageId, hash, userId)
+		if messageId == "1077041796779094096" then
+			local role = rolesIndex[hash]
+			if not role then return end
+			benbebot:getGuild("1068640496139915345"):getMember(userId):removeRole(role)
+		end
+	end
+	
+	benbebot:on("reactionAddUncached", add)
+	benbebot:on("reactionAdd", function(reaction, userId) add(reaction.message.channel, reaction.message.id, reaction.emojiHash, userId) end)
+	
+	benbebot:on("reactionRemoveUncached", remove)
+	benbebot:on("reactionRemove", function(reaction, userId) remove(reaction.message.channel, reaction.message.id, reaction.emojiHash, userId) end)
+	
 	-- server owner role
 	
 	local function add(guild)
