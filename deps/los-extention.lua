8cc44c72c931917a83fe5d4b7fd2ba89e36d3e2a
Benbebop <benbebop@hotmail.com>
2023-05-22 17:26:14 -0600

    support windows max name length

--- deps/los-extention.lua
+++ deps/los-extention.lua
@@ -3,7 +3,7 @@ local uv = require("uv")
 local los = require("los")
 
 function los.isProduction()
-	return uv.os_gethostname() == "benbebot-server-computer"
+	return uv.os_gethostname() == "benbebot-server"
 end
 
 return "test"
