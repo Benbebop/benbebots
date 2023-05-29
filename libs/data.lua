6a9a72af5c7ea49adf1d2ac40f2fd3b94678c9b6
Benbebop <benbebop@hotmail.com>
2023-05-29 13:24:11 -0600

    secret path

--- libs/data.lua
+++ libs/data.lua
@@ -2,24 +2,28 @@
 
 local fs, path = require("fs"), require("path")
 
-local PATH, TEMPPATH
+local PATH, TEMPPATH, SECRETPATH
 
 local opsys = require("los").type()
 if opsys == "win32" then
 	PATH = path.join(os.getenv( "LOCALAPPDATA" ), "/benbebots")
 	TEMPPATH = path.join(os.getenv( "TEMP" ), "/benbebots")
+	SECRETPATH = path.normalize(path.join(os.getenv( "LOCALAPPDATA" ), "/../../benbebot-secrets"))
 elseif opsys == "linux" then
 	PATH = path.join(os.getenv("HOME"), "/.benbebots")
 	TEMPPATH = "/var/tmp/benbebots"
+	SECRETPATH = path.join(os.getenv("HOME"), "/.benbebots-secrets")
 elseif opsys == "OSX" then -- i dont use OSX and dont care so idk if this works
 	PATH = "~/Library/Application Support/benbebots"
 	TEMPPATH = "~/Library/Caches/benbebots"
+	SECRETPATH = "~/Library/Application Support/benbebots-secrets"
 else
 	PATH = "data"
 	TEMPPATH = "data/temp"
+	SECRETPATH = "secret-data"
 end
 
-fs.mkdirSync(PATH) fs.mkdirSync(TEMPPATH)
+fs.mkdirSync(PATH) fs.mkdirSync(TEMPPATH) fs.mkdirSync(SECRETPATH)
 
 local data = {}
 
@@ -29,6 +33,10 @@ end
 
 function data.tempPath(rel)
 	return path.join(TEMPPATH, rel)
+end
+
+function data.secretPath(rel)
+	return path.join(SECRETPATH, rel)
 end
 
 return data
