b3b5f88cf7c7cdb10cf163f4a765cba9cb3809ea
Benbebop <78389958+Benbebop@users.noreply.github.com>
2023-10-14 00:13:59 -0400

    we benbebop-server up in here naw mean?

--- deps/los-extention.lua
+++ deps/los-extention.lua
@@ -3,7 +3,7 @@ local uv = require("uv")
 local los = require("los")
 
 function los.isProduction()
-	return uv.os_gethostname() == "benbebot-server"
+	return uv.os_gethostname() == "benbebop-server"
 end
 
 return "test"
