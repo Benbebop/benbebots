524825c94f714c15f20fc59851d5ad1aa8768d49
benbebop <benbebop@hotmail.com>
2024-01-05 16:15:26 -0500

    Initialise SQL

--- main.go
+++ main.go
@@ -6,60 +6,96 @@ import (
 	"os"
 
 	netrc "github.com/fhs/go-netrc/netrc"
+	"github.com/go-sql-driver/mysql"
 	"gopkg.in/ini.v1"
 )
 
 var cfg *ini.File
 var db *sql.DB
+var tokens = map[string]netrc.Machine{}
+
+func parseTokens() error {
+	mach, _, err := netrc.ParseFile("tokens.netrc")
+	if err != nil {
+		return err
+	}
+
+	for _, e := range mach {
+		tokens[e.Name] = *e
+	}
+	return nil
+}
+
+func parseConfig() error {
+	file, err := ini.LooseLoad("config.ini")
+	if err != nil {
+		return err
+	}
+
+	cfg = file
+	return nil
+}
+
+func connectDatabase(user string, passwd string) error {
+	sqlCfg := mysql.Config{
+		User:                 user,
+		Passwd:               passwd,
+		AllowNativePasswords: true,
+	}
+	database, err := sql.Open("mysql", sqlCfg.FormatDSN())
+	if err != nil {
+		return err
+	}
+	err = database.Ping()
+	if err != nil {
+		return err
+	}
+
+	db = database
+	return nil
+}
 
 func main() {
-	if os.Args[1] == "command-update" {
+	err := parseTokens()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	} else if os.Args[1] == "sql-update" {
-		sqlUpdate()
-	} else {
-		mach, _, err := netrc.ParseFile("tokens.netrc")
-		if err != nil {
-			log.Fatal(err)
-		}
+	err = parseConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		var tokens [4]netrc.Machine
-		for _, e := range mach {
-			switch e.Name {
-			case "benbebot":
-				tokens[0] = *e
-			case "familyguy":
-				tokens[1] = *e
-			case "gnerb":
-				tokens[2] = *e
-			case "cannedFood":
-				tokens[3] = *e
+	sqlToken, ok := tokens["sql"]
+	if !ok {
+		log.Println("WARNING: Missing sql token")
+	}
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "command-update" {
+			commandUpdate()
+		} else if os.Args[1] == "sql-update" {
+			if len(os.Args) > 2 {
+				err = connectDatabase("root", os.Args[2])
+			} else {
+				err = connectDatabase(sqlToken.Login, sqlToken.Password)
 			}
-		}
+			if err != nil {
+				log.Fatalln(err)
+			}
 
-		file, err := ini.LooseLoad("config.ini")
+			sqlUpdate()
+		}
+	} else {
+		err = connectDatabase(sqlToken.Login, sqlToken.Password)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalln(err)
 		}
 
-		cfg = file
+		go benbebot()
+		go familyguy()
 
-		db, err = sql.Open("mysql", "")
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = db.Ping()
-		if err == nil {
-			db = nil
-		} else {
-			log.Println(err)
-			return
-		}
-
-		benbebot(tokens[0].Password)
-		familyguy(tokens[1].Password)
-
-		gnerb(tokens[2].Password)
-		cannedfood()
+		go gnerb()
+		go cannedfood()
 	}
 }
--- update.go
+++ update.go
@@ -1,7 +1,95 @@
 package main
 
-func sqlUpdate() {
+import (
+	"log"
+)
 
+type sqlWarning struct {
+	level   string
+	code    uint16
+	message string
+}
+
+func sqlCatchWarning() sqlWarning {
+	row := db.QueryRow("SHOW WARNINGS")
+	var warn sqlWarning
+	row.Scan(&warn.level, &warn.code, &warn.message)
+	return warn
+}
+
+func sqlUpdate() {
+	row := db.QueryRow("SELECT USER()")
+	var username string
+	if err := row.Scan(&username); err == nil {
+		log.Printf("updating database as %s", username)
+	}
+
+	// TODO proper warning handling
+	// TODO compare table type
+	// benbebot user
+	_, err := db.Exec("CREATE USER IF NOT EXISTS 'benbebot'@'localhost' IDENTIFIED BY '" + tokens["sql"].Password + "'") // cant prepare a create user statement but we provide password so it should be fine
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1973 {
+		log.Println("added benbebot@localhost user")
+	}
+
+	// breadbag database
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS breadbagDiscord")
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1007 {
+		log.Println("added breadbagDiscord database")
+	}
+	_, err = db.Exec("GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT ON breadbagDiscord.* TO 'benbebot'@'localhost'")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("granted permissions for breadbagDiscord")
+	_, err = db.Exec("USE breadbagDiscord")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS vcHours (name CHAR(60), hours DOUBLE UNSIGNED)")
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1050 {
+		log.Println("added voice chat hours table")
+	}
+
+	// clips database
+	_, err = db.Exec("CREATE DATABASE IF NOT EXISTS discordClips")
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1007 {
+		log.Println("added discordClips database")
+	}
+	_, err = db.Exec("GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT ON discordClips.* TO 'benbebot'@'localhost'")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("granted permissions for discordClips")
+	_, err = db.Exec("USE discordClips")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS familyguy (id INT UNSIGNED NOT NULL AUTO_INCREMENT, upload VARCHAR(255), PRIMARY KEY (id))")
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1050 {
+		log.Println("added family guy clips table")
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS youngsheldon (id INT UNSIGNED NOT NULL AUTO_INCREMENT, upload VARCHAR(255), PRIMARY KEY (id))")
+	if err != nil {
+		log.Fatalln(err)
+	} else if sqlCatchWarning().code != 1050 {
+		log.Println("added young sheldon clips table")
+	}
+
+	db.Close()
+	log.Println("update complete")
 }
 
 func commandUpdate() {
