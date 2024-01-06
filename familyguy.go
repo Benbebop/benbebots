ba5813ca3ff9cca41d909476c5f7f86f72420383
benbebop <benbebop@hotmail.com>
2024-01-05 23:25:47 -0500

    merge family guy bots

--- familyguy.go
+++ familyguy.go
@@ -5,17 +5,9 @@ import (
 	"github.com/diamondburned/arikawa/v3/session"
 )
 
-func familyguy() {
-	client := session.New("Bot " + tokens["familyGuy"].Password)
+func familyguy(name string) {
+	client := session.New("Bot " + tokens[name].Password)
 	client.AddIntents(gateway.IntentGuildMembers) // privileged
-	client.AddHandler(createReadyAnnouncer(*client))
-
-	startSession(*client)
-}
-
-func familyguyTwo() {
-	client := session.New("Bot " + tokens["sheldon"].Password)
-	client.AddIntents(gateway.IntentGuildMembers | gateway.IntentMessageContent) // privileged
 	client.AddHandler(createReadyAnnouncer(*client))
 
 	startSession(*client)
