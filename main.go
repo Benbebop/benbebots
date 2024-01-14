12dde0982fb7758edc91f58e48b75c8d32863aeb
benbebop <benbebop@hotmail.com>
2024-01-14 04:37:34 -0500

    better error reporting

--- main.go
+++ main.go
@@ -3,14 +3,80 @@ package main
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"runtime"
+	"strconv"
+	"time"
 
+	"github.com/diamondburned/arikawa/v3/api"
+	"github.com/diamondburned/arikawa/v3/discord"
 	netrc "github.com/fhs/go-netrc/netrc"
 	"github.com/go-sql-driver/mysql"
 	"gopkg.in/ini.v1"
 )
+
+func writeErrorLog(inErr error) (string, error) {
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		return "", err
+	}
+	id := strconv.FormatInt(time.Now().UnixMicro(), 36)
+	os.Mkdir(dir+"/benbebots/errors/", fs.FileMode(0777))
+	file, err := os.OpenFile(fmt.Sprintf("%s/benbebots/errors/%s.log", dir, id), os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		return "", err
+	}
+	_, err = file.Write([]byte(inErr.Error() + "\n\n"))
+	if err != nil {
+		return "", err
+	}
+	b := make([]byte, 2048)
+	n := runtime.Stack(b, false)
+	_, err = file.Write(b[:n])
+	if err != nil {
+		return "", err
+	}
+	file.Close()
+	return id, nil
+}
+
+func cmdErrorResp(inErr error) *api.InteractionResponseData {
+	id, err := writeErrorLog(inErr)
+	if err != nil {
+		log.Println(err)
+		id = "no id"
+	}
+
+	var stk string
+	for i := 1; i < 6; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		stk += "\n"
+		if !ok {
+			stk += "..."
+			break
+		}
+		stk += fmt.Sprintf("%s: %d 0x%x", file, line, pc)
+	}
+
+	return &api.InteractionResponseData{
+		Flags: discord.EphemeralMessage,
+		Embeds: &[]discord.Embed{
+			{
+				Author: &discord.EmbedAuthor{
+					Name: "There was an error!",
+				},
+				Title:       id,
+				Description: inErr.Error(),
+				Footer: &discord.EmbedFooter{
+					Text: stk,
+				},
+			},
+		},
+	}
+}
 
 // startup //
 
@@ -75,14 +141,14 @@ func getDirs() error {
 	}
 	dirs.Data = dir + "/benbebots/"
 	if _, err := os.Stat(dirs.Data); errors.Is(err, os.ErrNotExist) {
-		return os.Mkdir(dirs.Data, fs.FileMode(0777))
+		os.Mkdir(dirs.Data, fs.FileMode(0777))
 	} else if err != nil {
 		return err
 	}
 
 	dirs.Temp = os.TempDir() + "/benbebots/"
 	if _, err := os.Stat(dirs.Data); errors.Is(err, os.ErrNotExist) {
-		return os.MkdirAll(dirs.Data, fs.FileMode(0777))
+		os.MkdirAll(dirs.Data, fs.FileMode(0777))
 	} else if err != nil {
 		return err
 	}
