79206a4cfd7ad56d5db1bbcfa2e7e52248d78c97
benbebop <benbebop@hotmail.com>
2024-02-24 19:58:59 -0800

    move initializers out of functions

--- main.go
+++ main.go
@@ -92,33 +92,6 @@ var dirs struct {
 	Temp string
 }
 
-func parseTokens() error {
-	mach, _, err := netrc.ParseFile("tokens.netrc")
-	if err != nil {
-		return err
-	}
-
-	for _, e := range mach {
-		tokens[e.Name] = *e
-	}
-	return nil
-}
-
-func parseConfig() error {
-	var err error
-	cfg, err = ini.LoadSources(ini.LoadOptions{
-		Loose:                     true,
-		Insensitive:               true,
-		UnescapeValueDoubleQuotes: true,
-		AllowShadows:              true,
-	}, "config.ini")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func connectDatabase(user string, passwd string) error {
 	sqlCfg := mysql.Config{
 		User:                 user,
@@ -138,45 +111,50 @@ func connectDatabase(user string, passwd string) error {
 	return nil
 }
 
-func getDirs() error {
+var botGoroutineGroup sync.WaitGroup
+
+func main() {
+	// parse tokens
+	mach, _, err := netrc.ParseFile("tokens.netrc")
+	if err != nil {
+		return
+	}
+
+	for _, e := range mach {
+		tokens[e.Name] = *e
+	}
+
+	// parse config
+	cfg, err = ini.LoadSources(ini.LoadOptions{
+		Loose:                     true,
+		Insensitive:               true,
+		UnescapeValueDoubleQuotes: true,
+		AllowShadows:              true,
+	}, "config.ini")
+	if err != nil {
+		return
+	}
+
+	// initialise data directories
 	dir, err := os.UserCacheDir()
 	if err != nil {
-		return err
+		return
 	}
 	dirs.Data = dir + "/benbebots/"
 	if _, err := os.Stat(dirs.Data); errors.Is(err, os.ErrNotExist) {
 		os.Mkdir(dirs.Data, fs.FileMode(0777))
 	} else if err != nil {
-		return err
+		return
 	}
 
 	dirs.Temp = os.TempDir() + "/benbebots/"
 	if _, err := os.Stat(dirs.Data); errors.Is(err, os.ErrNotExist) {
 		os.MkdirAll(dirs.Data, fs.FileMode(0777))
 	} else if err != nil {
-		return err
-	}
-	return nil
-}
-
-var botGoroutineGroup sync.WaitGroup
-
-func main() {
-	err := parseTokens()
-	if err != nil {
-		log.Fatalln(err)
+		return
 	}
 
-	err = parseConfig()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = getDirs()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+	// read args
 	argLen := len(os.Args)
 	if argLen > 1 {
 		if os.Args[1] == "update-commands" {
