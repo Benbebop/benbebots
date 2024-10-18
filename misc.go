26e6bb24cdc56a6fcf2e75d2e34db986e2184288
Benbebop <benbebop@hotmail.com>
2024-10-17 18:51:35 -0600

    move fnaf bot back to misc

--- misc.go
+++ misc.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"math/rand"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
+	"benbebop.net/benbebots/internal/scheduler"
 	"benbebop.net/benbebots/internal/stats"
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
 	"github.com/diamondburned/arikawa/v3/session"
+	"github.com/diamondburned/arikawa/v3/utils/sendpart"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
@@ -286,5 +291,66 @@ func (Benbebots) CANNEDFOOD() *session.Session {
 	})
 
 	logs.Assert(client.Open(client.Context()))
+	return client
+}
+
+type FnafConfig struct {
+	Source      string            `toml:"source"`
+	Time        time.Duration     `toml:"pou_time"`
+	Destination discord.ChannelID `toml:"destination"`
+	StatChannel discord.ChannelID `toml:"stat_channel"`
+}
+
+func (Benbebots) FNAF() *api.Client { // gnerb
+	if !config.Components.IsEnabled("gnerb") {
+		logs.Info("gnerb component has been disabled")
+		return nil
+	}
+
+	client := api.NewClient("Bot " + tokens["fnaf"].Password)
+
+	fnafStat := stats.Stat{
+		Name:      "Gnerbs",
+		Value:     0,
+		Client:    client,
+		ChannelID: config.Bot.Fnaf.StatChannel,
+		LevelDB:   lvldb,
+		Delay:     time.Second * 5,
+	}
+	fnafStat.Initialise()
+
+	ext := filepath.Ext(config.Bot.Fnaf.Source)
+	if ext != ".png" && ext != ".jpg" && ext != ".webp" {
+		logs.Fatal("invalid gnerb format")
+	}
+
+	content, err := os.ReadFile(config.Bot.Fnaf.Source)
+	if err != nil {
+		logs.Fatal("%s", err)
+	}
+
+	go func() {
+		for {
+			wait := scheduler.TimeToDaily(config.Bot.Fnaf.Time)
+			logs.Info("sending next pou image in %fh", wait.Hours())
+			time.Sleep(wait)
+
+			client.SendMessageComplex(config.Bot.Fnaf.Destination, api.SendMessageData{
+				Files: []sendpart.File{
+					{
+						Name:   "image" + ext,
+						Reader: bytes.NewReader(content),
+					},
+				},
+			})
+		}
+	}()
+
+	me, err := client.Me()
+	if err == nil {
+		AnnounceReady(&gateway.ReadyEvent{
+			User: *me,
+		})
+	}
 	return client
 }
