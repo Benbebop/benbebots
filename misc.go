d8e9f8faade827b2cc713a04ca9212f6bbb1ef1e
Benbebop <benbebop@hotmail.com>
2024-11-08 19:04:12 -0700

    don cheadle bot improvements

--- misc.go
+++ misc.go
@@ -3,10 +3,15 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
+	"io"
 	"math/rand"
+	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -349,5 +354,100 @@ func (Benbebots) FNAF() *api.Client { // gnerb
 			User: *me,
 		})
 	}
+	return client
+}
+
+const RANDOM_WORD_URL = "https://random-word-api.herokuapp.com/word"
+
+type DonCheadleConfig struct {
+	Font     string            `toml:"font_file"`
+	Source   string            `toml:"src"`
+	SendTime time.Duration     `toml:"send_time"`
+	Channel  discord.ChannelID `toml:"channel"`
+}
+
+var wordSteriliser, _ = regexp.Compile("[^a-zA-Z]")
+
+func (Benbebots) DONCHEADLE() *api.Client {
+	if !config.Components.IsEnabled("don_cheadle") {
+		log.Info("don cheadle component has been disabled")
+		return nil
+	}
+	client := api.NewClient("Bot " + tokens["doncheadle"].Password)
+
+	var textArgs = "drawtext=fontcolor=white:borderw=4:fontsize=62:fontfile='" + config.Bot.DonCheadle.Font + "':x=(w-text_w)/2"
+
+	go func() {
+		var release io.Closer
+		for {
+			if release != nil {
+				release.Close()
+				release = nil
+			}
+			wait := scheduler.TimeToDaily(config.Bot.DonCheadle.SendTime)
+			log.Info("sending next don cheadle wotd in %fh", wait.Hours())
+			time.Sleep(wait)
+
+			resp, err := http.Get(RANDOM_WORD_URL)
+			if err != nil {
+				log.ErrorQuick(err)
+				continue
+			}
+			release = resp.Body
+
+			var raw bytes.Buffer
+			var words []string
+			s, _ := log.Assert(json.NewDecoder(io.TeeReader(resp.Body, &raw)).Decode(&words))
+			if s {
+				continue
+			} else if len(words) <= 0 {
+				resp.Body = io.NopCloser(bytes.NewReader(raw.Bytes()))
+				log.DumpResponse(resp, true, 2, "no words returned")
+				continue
+			}
+			word := wordSteriliser.ReplaceAllString(words[0], "")
+
+			c := exec.Command(config.Programs.FFMpeg,
+				"-i", config.Bot.DonCheadle.Source,
+				"-vf", "[in]"+textArgs+":y=14:text='DON CHEADLE',"+
+					textArgs+":y=94:text='WORD OF THE DAY',"+
+					textArgs+":y=(h)-76:text='"+strings.ToUpper(word)+"'"+"[out]",
+				"-f", "mjpeg",
+				"-",
+			)
+
+			out, err := c.StdoutPipe()
+			if err != nil {
+				log.ErrorQuick(err)
+				continue
+			}
+
+			err = c.Start()
+			if err != nil {
+				log.ErrorQuick(err)
+				continue
+			}
+
+			_, err = client.SendMessageComplex(config.Bot.DonCheadle.Channel, api.SendMessageData{
+				Files: []sendpart.File{
+					{
+						Name:   words[0] + ".jpg",
+						Reader: out,
+					},
+				},
+			})
+			if err != nil {
+				log.ErrorQuick(err)
+				continue
+			}
+
+			err = c.Wait()
+			if err != nil {
+				log.ErrorQuick(err)
+				continue
+			}
+		}
+	}()
+
 	return client
 }
